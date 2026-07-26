44980a4f748adea6004fce5a8057ead40cf66659
guigui-will <guigui.2535.secundario@gmail.com>
2026-07-26 02:29:23 +0100

    Add DenseLayer class for fully connected layer

--- DenseLayer.hpp
+++ DenseLayer.hpp
@@ -1,1 +1,25 @@
+#pragma once
+#include "Matrix.hpp"
+#include "Activation.hpp"
 
+// Camada totalmente conectada: entrada -> saida, com pesos, bias e ativação.
+// As matrizes de entrada/saida usam colunas como amostras (cada coluna = 1 exemplo do batch).
+class DenseLayer {
+public:
+    DenseLayer(int inputSize, int outputSize, ActivationType activation);
+
+    Matrix forward(const Matrix& input);
+    // recebe dLoss/dOutput e devolve dLoss/dInput, atualizando pesos e bias
+    Matrix backward(const Matrix& dOutput, double learningRate);
+
+private:
+    int inputSize_, outputSize_;
+    ActivationType activation_;
+
+    Matrix weights_;   // outputSize x inputSize
+    Matrix bias_;      // outputSize x 1
+
+    // cache para o backward
+    Matrix lastInput_;
+    Matrix lastZ_;     // pre-ativação
+};
