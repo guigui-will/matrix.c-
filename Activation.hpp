f07144b4506c6148dfa3289eaa4e55bbe70a99ae
guigui-will <guigui.2535.secundario@gmail.com>
2026-07-26 02:27:34 +0100

    Add Activation class with types and methods

--- Activation.hpp
+++ Activation.hpp
@@ -1,1 +1,15 @@
+#pragma once
+#include "Matrix.hpp"
 
+enum class ActivationType {
+    Sigmoid,
+    ReLU,
+    Tanh,
+    Linear
+};
+
+class Activation {
+public:
+    static Matrix apply(const Matrix& z, ActivationType type);
+    static Matrix derivative(const Matrix& z, ActivationType type);
+};
